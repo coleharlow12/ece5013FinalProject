a235d10377bb8fb1e253addf95b76e2e3162fbad
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-04-27 16:06:20 -0400

    making code more readbale

--- samplecode.m
+++ samplecode.m
@@ -4,21 +4,16 @@ M=256; % number of pulses in one CPI
 N_cpi= 32; % number of CPIs in data
 
 
-   
-  
 for ii=1:N_cpi;
     phase_history1=zeros(301,M);
     phase_history2=zeros(301,M);
     
     for jj=1:M
-  phase_history1(:,jj)=fread(fid,301,'double');
-  phase_history1(:,jj)=phase_history1(:,jj)+1i*fread(fid,301,'double');
-  
-  phase_history2(:,jj)=fread(fid,301,'double');
-  phase_history2(:,jj)=phase_history2(:,jj)+1i*fread(fid,301,'double');
+          phase_history1(:,jj)=fread(fid,301,'double');
+          phase_history1(:,jj)=phase_history1(:,jj)+1i*fread(fid,301,'double');
 
-
-  
+          phase_history2(:,jj)=fread(fid,301,'double');
+          phase_history2(:,jj)=phase_history2(:,jj)+1i*fread(fid,301,'double');
     end;
   
    
@@ -33,12 +28,6 @@ for ii=1:N_cpi;
    % Construct Range Doppler map from the two antennas and display.
    % Consider canceling stationary clutter (two/three pulse canceller, or 
    % subtracting average (across slow time) range-profile from each return 
-   
-   
-
-
-
-
 end
 
 fclose(fid);
