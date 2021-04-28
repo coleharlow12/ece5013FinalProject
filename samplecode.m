022da1ffa006de3623a32729929ea2be8f0935cb
coleharlow12 <33840536+coleharlow12@users.noreply.github.com>
2021-04-28 15:51:00 -0400

    Before merge

--- samplecode.m
+++ samplecode.m
@@ -4,7 +4,7 @@ M=256; % number of pulses in one CPI
 N_cpi= 32; % number of CPIs in data
 
 
-for ii=1:N_cpi;
+for ii=1:N_cpi
     phase_history1=zeros(301,M);
     phase_history2=zeros(301,M);
     
@@ -14,7 +14,7 @@ for ii=1:N_cpi;
 
           phase_history2(:,jj)=fread(fid,301,'double');
           phase_history2(:,jj)=phase_history2(:,jj)+1i*fread(fid,301,'double');
-    end;
+    end
   
    
    %only Range bins >=131 have target information, earlier range bins are
@@ -26,6 +26,7 @@ for ii=1:N_cpi;
     
    % phase_history matrix: row range-bins 0:170, columns :pulse_no 1:M
    % Construct Range Doppler map from the two antennas and display.
+   
    % Consider canceling stationary clutter (two/three pulse canceller, or 
    % subtracting average (across slow time) range-profile from each return 
 end
